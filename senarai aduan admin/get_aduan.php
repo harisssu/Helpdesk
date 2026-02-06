0fc8e66f40148334effe4f2ec15bf35a4c07999f
balqis17project <a.blqss.1725@gmail.com>
2026-02-06 09:15:01 +0800

    Update get_aduan.php

--- senarai aduan admin/get_aduan.php
+++ senarai aduan admin/get_aduan.php
@@ -15,15 +15,8 @@ $sql = "
         a.jenisMasalah,
         a.tarikhAduan,
         a.masaAduan,
-
-        /* noIC boleh NULL sebab ketua unit guna noICKetua */
         a.noIC,
-        a.noICKetua,
-
-        /* Nama pengadu & jabatan */
-        COALESCE(u.namaUser, k.namaKetua, '-') AS namaPengadu,
-        COALESCE(ju.namaJabatan, jk.namaJabatan, '-') AS namaJabatan,
-
+        u.namaUser,
         a.lokasi,
         a.peneranganMasalah,
         a.attachment,
@@ -31,19 +24,12 @@ $sql = "
         a.noICTechnician
     FROM aduan a
     LEFT JOIN user u ON a.noIC = u.noIC
-    LEFT JOIN jabatan ju ON u.idJabatan = ju.idJabatan
-
-    LEFT JOIN ketuaunit k ON a.noICKetua = k.noICKetua
-    LEFT JOIN jabatan jk ON k.idJabatan = jk.idJabatan
-
     WHERE a.idAduan = ?
 ";
 
-
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $idAduan);
+$stmt->bind_param("s", $idAduan);
 $stmt->execute();
 
 $result = $stmt->get_result();
 echo json_encode($result->fetch_assoc());
-exit;
